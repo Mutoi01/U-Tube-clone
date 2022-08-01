c52eafa80186e716729ee5bc877393f160a0bbbe
Mutoi01 <pngando9@gmail.com>
2022-08-01 05:16:17 +0300

    add contact page

---
+++ src/components/ContactUs.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../App.css';
+import NavBar from '../components/NavBar';
+
+
+function ContactUs (){
+
+
+  return ( 
+    <>
+
+    {/* <NavBar /> */}
+
+   <div className="container" id="contact">
+
+     <h1>U-Tube Help Center </h1>
+
+
+     <p>While we're good with smoke signals, there are simpler ways for us to get in touch and answer your questions.</p>
+    
+     <p>Need something? We are here to help!</p>
+
+     <p>Search our FAQs for answers to anything you might be wondering</p>
+
+     <input type='text' placeholder= "Search our articles" search-bar ui segment />
+     <button type="button" className="btn btn-dark" >Search</button>
+
+
+     {/* <a href="https://github.com/Mutoi01/U-Tube"> Twitter | Instagram | Discord </a> */}
+     
+   </div>
+
+   </>
+  )
+}
+
+export default ContactUs
