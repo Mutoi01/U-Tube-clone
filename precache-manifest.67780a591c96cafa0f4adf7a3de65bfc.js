340033045be4aef0d53d212f1f7939cad07f019d
Mutoi01 <pngando9@gmail.com>
2022-08-01 06:32:25 +0300

    Updates

---
+++ precache-manifest.67780a591c96cafa0f4adf7a3de65bfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5706f5b64f572de9c451fb19a0ce55",
+    "url": "/U-Tube-clone/index.html"
+  },
+  {
+    "revision": "637c033a5a38a8bbd38f",
+    "url": "/U-Tube-clone/static/css/main.a3b5c6a3.chunk.css"
+  },
+  {
+    "revision": "1feb7bc197c84d8cefc0",
+    "url": "/U-Tube-clone/static/js/2.1b0c3a55.chunk.js"
+  },
+  {
+    "revision": "1aacdb255c41a16e147dbd73726fbe15",
+    "url": "/U-Tube-clone/static/js/2.1b0c3a55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637c033a5a38a8bbd38f",
+    "url": "/U-Tube-clone/static/js/main.3cce4a69.chunk.js"
+  },
+  {
+    "revision": "8399d9c071cd6aa0195f",
+    "url": "/U-Tube-clone/static/js/runtime-main.98d14c75.js"
+  },
+  {
+    "revision": "37c790a0c15725fbbcab556c863ab06b",
+    "url": "/U-Tube-clone/static/media/youuttuubbee.37c790a0.jpg"
+  }
+]);
