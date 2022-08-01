051e28b490077dd6facdc781e994f4b9105828c9
Mutoi01 <pngando9@gmail.com>
2022-08-01 05:19:01 +0300

    add fetch using axios

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import youtube, { KEY } from '../api/ytapi'
+import { SearchForm } from './SearchForm'
+import { VideoList } from './VideoList'
+import { VideoDetail } from './VideoDetail'
+
+export const App = () => {
+    const [videos, setVideos] = useState([])
+    const [selectedVideo, setSelectedVideo] = useState(null)
+
+
+    const onSearchSubmit = async (searchTerm) => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: searchTerm,
+                part: 'snippet',
+                maxResults: 6,
+                key: KEY
+            }
+        })
+        const responseArr = response.data.items;
+        const firstVideo = responseArr[0].id.kind !== 'youtube#channel' ? responseArr[0] : responseArr[1]
+        setVideos(responseArr)
+        setSelectedVideo(firstVideo)
+
+    }
+
+    return KEY === '' ? <h1> </h1> :
+        <div className="ui container">
+            <SearchForm onSearchSubmit={onSearchSubmit} />
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail video={selectedVideo}></VideoDetail>
+                    </div>
+                    <div className="five wide column">
+                        <VideoList videos={videos} setSelectedVideo={setSelectedVideo} />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+} 
