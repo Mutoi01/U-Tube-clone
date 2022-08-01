f6b479c501db1ca8a72634287c1723f81523975d
Mutoi01 <pngando9@gmail.com>
2022-08-01 05:22:14 +0300

    add playing  video description

---
+++ src/components/VideoItem.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './VideoItem.css'
+export const VideoItem = ({ video, setSelectedVideo }) => {
+
+    return (
+        <div className="video-item item" onClick={() => setSelectedVideo(video)}>
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+                {/* <div className="description"> {video.snippet.description}</div> */}
+            </div>
+        </div>
+    );
+}
